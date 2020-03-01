01435fa9d3f74f74af3fb7b205186f4f23ddd968
Gmangray4 <31903322+Gmangray4@users.noreply.github.com>
2020-03-01 13:37:56 -0500

    MoreThings added

---
+++ Prototype1/Assets/Scripts/EatAnythingOnContact.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eat : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject)
+        {
+            Debug.Log("Collision Decected");
+            Destroy(col.gameObject);
+        }
+    }
+
+}
---
+++ Prototype1/Assets/Scripts/EatAnythingWithName.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eat : MonoBehaviour
+{
+    [SerializeField] private Transform Player;
+    [SerializeField] private Transform respawnPoint;
+
+    void OnTriggerEnter(Collider Player)
+    {
+        Debug.Log("OnCollisionEnter works.");
+        if (Player.gameObject.name == "FPSController")
+        {
+            Debug.Log("The GameObject name is Cube.");
+            Player.transform.position = respawnPoint.transform.position;
+         
+        }
+    }
+
+}
