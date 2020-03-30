0d9916f529362e4f5df86eefd5512b5373c734c2
Gmangray4 <31903322+Gmangray4@users.noreply.github.com>
2020-03-29 20:29:59 -0400

    Added Respwan for Head and Head can kill player now

---
+++ Prototype3_V3/Assets/RespawnHead.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnHead : MonoBehaviour
+{
+    [SerializeField] private Transform Player;
+    [SerializeField] private Transform Head;
+    [SerializeField] private Transform respawnPoint;
+
+    void OnTriggerEnter(Collider Player)
+    {
+        Debug.Log("OnCollisionEnter works.");
+        if (Player.gameObject.name == "Player")
+        {
+            Debug.Log("The GameObject name is Player.");
+            Head.transform.position = respawnPoint.transform.position;
+
+        }
+    }
+
+}
