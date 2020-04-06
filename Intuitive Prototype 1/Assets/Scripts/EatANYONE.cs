47c675a798662dec04b0d45c8d8b168ece5bcc4f
Gordon Gray <31903322+Gmangray4@users.noreply.github.com>
2020-04-06 11:35:34 -0400

    TimmerAdded!

--- Intuitive Prototype 1/Assets/Scripts/EatANYONE.cs
+++ Intuitive Prototype 1/Assets/Scripts/EatANYONE.cs
@@ -10,27 +10,34 @@ public class EatANYONE : MonoBehaviour
     public int theScore; 
     public AudioSource CakeEatSound;
 
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+      
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+     
     }
 
     void OnCollisionEnter(Collision col)
     {
-        if (col.gameObject && col.gameObject.name != "Terrain" && col.gameObject.name != "Oven1" && col.gameObject.name != "Oven2" && col.gameObject.name != "Oven3" && col.gameObject.name != "Oven4" && col.gameObject.name != "Oven5" && col.gameObject.name != "Oven6" && col.gameObject.name != "OutSideWall")
+        if (col.gameObject && col.gameObject.name != "Terrain" && col.gameObject.name != "Oven1" && col.gameObject.name != "Oven2" && col.gameObject.name != "Oven3" && col.gameObject.name != "Oven4" && col.gameObject.name != "Oven5" && col.gameObject.name != "Oven6" && col.gameObject.name != "OutSideWall" && col.gameObject.name != "GGrayHead")
         {
             Debug.Log("Collision Decected");
+            CakeEatSound.Play();
             theScore += 10;
             ScoreText.GetComponent<Text>().text = "Score: " + theScore;
             Destroy(col.gameObject);
         }
+        if(col.gameObject && col.gameObject.name == "GGrayHead")
+        {
+            theScore -= 10;
+            ScoreText.GetComponent<Text>().text = "Score: " + theScore;
+        }
     }
-
 }
--- Intuitive Prototype 1/Assets/Scripts/KillPlayer.cs
+++ Intuitive Prototype 1/Assets/Scripts/KillPlayer.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class KillPlayer : MonoBehaviour
 {
     [SerializeField] private Transform Player;
     [SerializeField] private Transform respawnPoint;
+ 
 
     void OnTriggerEnter(Collider Player)
     {
@@ -13,6 +15,8 @@ public class KillPlayer : MonoBehaviour
         if (Player.gameObject.name == "Player")
         {
             Debug.Log("The GameObject name is Player.");
+      
+            
             Player.transform.position = respawnPoint.transform.position;
          
         }
