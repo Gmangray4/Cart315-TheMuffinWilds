cd66f47c1f04f611097b1bf78f0a7dadd8acea2e
Gordon Gray <31903322+Gmangray4@users.noreply.github.com>
2020-04-05 22:58:54 -0400

    AI Head added

---
+++ Prototype3_V3/Assets/Scripts/HeadKillPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadKillPlayer : MonoBehaviour
+{
+    [SerializeField] private Transform Player;
+    [SerializeField] private Transform respawnPoint;
+
+    void OnTriggerEnter(Collider Player)
+    {
+        Debug.Log("OnCollisionEnter works.");
+        if (Player.gameObject.name == "Player")
+        {
+            Debug.Log("The GameObject name is Player.");
+            Player.transform.position = respawnPoint.transform.position;
+         
+        }
+    }
+
+}
